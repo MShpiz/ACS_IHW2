e5feb501bdc1f3a80b42afb75849dd666ac48caa
Panfilova Maria <reg@panfilova.com>
2023-11-03 20:26:59 +0300

    changed interval points to int

--- code/user_input.asm
+++ code/user_input.asm
@@ -8,18 +8,23 @@ user_input_integral:
 	print_str("function y = a + b*x^3 is going to be inegrated \n")
 	
 	#getting a and b from user
-	print_str("enter a:")
+	print_str("enter a (a real number):")
 	read_double(fa0)
 	fpush(fa0)	# pushing to stack because print macros uses a0 register
-	print_str("enter b:")
+	print_str("enter b (a real number):")
 	read_double(fa1)
 	
 	#getting interval of integration from user
 	print_str("enter the interval of inegration \n")
-	print_str("enter starting point: ")
-	read_double(fa2)
-	print_str("enter ending point: ")
-	read_double(fa3)
+	print_str("enter starting point (should be integer): ")
+	read_int(t2)
+	print_str("enter ending point (should be integer): ")
+	read_int(t3)
+	
+	fcvt.d.w	fa2, t2		# converting ends of interval to double
+	fcvt.d.w	fa3, t3
+	
+	
 	fpop(fa0)	# getting a from stack
 	
 	# returns a - a0
